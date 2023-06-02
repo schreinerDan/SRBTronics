a4ac8a1cc5117b221af6c1e6e6b21b9b1b383ed3
schreinerDan <schreiner.daniel@gmail.com>
2023-06-02 15:39:19 -0300

    Update srbtmachine.cpp

--- srbtmachine.cpp
+++ srbtmachine.cpp
@@ -49,39 +49,30 @@ void SRBTMachine::AddModule(SRBTModule *_module)
 {
     this->PrintSR("a");
     _module->moduleID=this->_moduleCount;
-    //SRBTMachine::PrintSR((String)this->_moduleCount);
     this->listModules[this->_moduleCount]=_module;
-    //this->listModules[this->_moduleCount]=_module;
-    //this->listModules[this->_moduleCount]=_module;
     this->_moduleCount++;
 }
 
 void SRBTMachine::Update()
 {
-    //if (!this->activeModule==NULL)
-    //this->activeModule->UpdateModule();
 
     this->listModules[this->_moduleIndex]->UpdateModule();
 
-    /*for(int i=0;i<this->_moduleCount;i++)
-    {
-        this->listModules[i]->UpdateModule();
-    }*/
 }
 void SRBTMachine::CheckCommand(String _cmd)
 {
-    //this->instance()->PrintSR("Ck BvM "+_cmd);
+
     this->instance()->listModules[this->_moduleIndex]->CheckCommand(_cmd);
 
     for(int i=0;i<this->_behaviorCount;i++)
     {
         this->listBehavior[i]->CheckCommand(_cmd);
-        //this->instance()->PrintSR("Ck BvM "+((String)i));
+
     }
     for(int i=0;i<this->_behaviorSMCount;i++)
     {
         this->listBehaviorSenMachine[i]->Update();
-        //this->instance()->PrintSR("Ck BvM "+((String)i));
+
     }
 
 }
